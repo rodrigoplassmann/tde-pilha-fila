781ba748ce07d8c1785ad9fa5c2d82228f21b2ac
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-10-19 11:56:00 -0300

    Criação da classe Elemento

---
+++ src/Elemento.java
@@ -0,0 +1,23 @@
+public class Elemento {
+    private String idSolicitacao;
+    private String descricaoSolicitacao;
+    private String dataHora;
+
+    private String nomeCliente;
+    private String idCliente;
+    private String motivoAtendimento;
+
+    /*
+    public Elemento(String idSolicitacao, String descricaoSolicitacao, String dataHora){
+        this.idSolicitacao = idSolicitacao;
+        this.descricaoSolicitacao = descricaoSolicitacao;
+        this.dataHora = dataHora;
+    }
+
+    public Elemento(String nomeCliente, String idCliente, String motivoAtendimento){
+        this.nomeCliente = nomeCliente;
+        this.idCliente = idCliente;
+        this.motivoAtendimento = motivoAtendimento;
+    }
+     */
+}
