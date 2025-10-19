e3310dd4ff6cf6442a6242e70e50d0b873349ffa
Rodrigo Plassmann <rodrigo.plassmann@hotmail.com>
2025-10-19 12:01:47 -0300

    Getters e setters dos atributos da classe Elemento

--- src/Elemento.java
+++ src/Elemento.java
@@ -20,4 +20,52 @@ public class Elemento {
         this.motivoAtendimento = motivoAtendimento;
     }
      */
+
+    public String getIdSolicitacao() {
+        return idSolicitacao;
+    }
+
+    public void setIdSolicitacao(String idSolicitacao) {
+        this.idSolicitacao = idSolicitacao;
+    }
+
+    public String getDescricaoSolicitacao() {
+        return descricaoSolicitacao;
+    }
+
+    public void setDescricaoSolicitacao(String descricaoSolicitacao) {
+        this.descricaoSolicitacao = descricaoSolicitacao;
+    }
+
+    public String getDataHora() {
+        return dataHora;
+    }
+
+    public void setDataHora(String dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getMotivoAtendimento() {
+        return motivoAtendimento;
+    }
+
+    public void setMotivoAtendimento(String motivoAtendimento) {
+        this.motivoAtendimento = motivoAtendimento;
+    }
 }
